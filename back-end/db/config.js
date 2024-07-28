e15eab539060cb06d41888e2e560f08c3f07c970
Aditya030an <adityaanu20023@gmail.com>
2024-07-28 15:12:47 +0530

    mongodb atlas

--- back-end/db/config.js
+++ back-end/db/config.js
@@ -1,5 +1,22 @@
-const mongoos = require("mongoose");
-mongoos.connect("mongodb://localhost:27017/trip-planner-ai");
+// const mongoose = require("mongoose");
+// // mongoos.connect("mongodb+srv://adityaanu20023:Aditya@123@cluster0.xt80gyk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 
-//mongodb+srv://adityaanu20023:Aditya@123@cluster0.zx6n2ny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// const mongoURI = "mongodb+srv://adityaanu20023:Aditya@123@cluster0.xt80gyk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+// mongoose.connect(mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("Failed to connect to MongoDB", err));
+
+// //mongodb+srv://adityaanu20023:Aditya@123@cluster0.zx6n2ny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
--- back-end/index.js
+++ back-end/index.js
@@ -1,19 +1,97 @@
+// const express = require("express");
+// const cors = require("cors");
+// require("./db/config");
+// const User = require("./db/User");
+// const TripData = require("./db/TripData");
+// const app = express();
+// require('dotenv').config();
+
+// // const mongoURI = process.env.MONGODB_URI;
+
+// // mongoose.connect(mongoURI, {
+// //   useNewUrlParser: true,
+// //   useUnifiedTopology: true,
+// // })
+// // .then(() => console.log("Connected to MongoDB"))
+// // .catch((err) => console.error("Failed to connect to MongoDB", err));
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.post("/register", async (req, res) => {
+//   let user = new User(req.body);
+//   let result = await user.save();
+//   result = result.toObject();
+//   delete result.password;
+//   if (result) {
+//     res.send(result);
+//   }
+// });
+
+// app.post("/login", async (req, res) => {
+//   if (req.body.email && req.body.password) {
+//     let user = await User.findOne(req.body).select("-password");
+//     if (user) {
+//       res.send(user);
+//     } else {
+//       res.send({ result: "No user found" });
+//     }
+//   }
+// });
+
+// app.get("/register/:key", async (req, res) => {
+//   // res.send("login key works" ,);
+//   let result = await User.find({
+//     email: req.params.key,
+//   });
+//   if (result.length > 0) {
+//     res.send(result);
+//   } else {
+//     res.send({ result: "No user found" });
+//   }
+// });
+
+// app.post("/tripdata", async (req, res) => {
+//   // res.send("trip data working...");
+//   let data = new TripData(req.body);
+//   console.log("in backend req", req.body);
+//   let result = await data.save();
+//   res.send(result);
+//   console.log("in backend", result);
+// });
+
+// app.get("/view-trip/:id", async (req, res) => {
+//   let result = await TripData.findOne({ docID: req.params.id });
+//   if (result) {
+//     res.send(result);
+//   } else {
+//     res.send({ result: "No found trip" });
+//   }
+// });
+
+// app.get("/my-trip/:key", async (req, res) => {
+//   // res.send("my trip working");
+//   let result = await TripData.find({ userEmail: req.params.key });
+//   res.send(result);
+// });
+
+// // app.listen("https://trip-planner-ai-back-end.vercel.app/"||5000);
+
+// // const PORT = process.env.PORT || 5000;
+// // app.listen(PORT, () => {
+// //   console.log(`Server is running on port ${PORT}`);
+// // });
+
+// app.listen(5000);
+
+
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 require("./db/config");
 const User = require("./db/User");
 const TripData = require("./db/TripData");
 const app = express();
-require('dotenv').config();
-
-const mongoURI = process.env.MONGODB_URI;
-
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.error("Failed to connect to MongoDB", err));
 
 app.use(express.json());
 app.use(cors());
@@ -40,7 +118,6 @@ app.post("/login", async (req, res) => {
 });
 
 app.get("/register/:key", async (req, res) => {
-  // res.send("login key works" ,);
   let result = await User.find({
     email: req.params.key,
   });
@@ -52,7 +129,6 @@ app.get("/register/:key", async (req, res) => {
 });
 
 app.post("/tripdata", async (req, res) => {
-  // res.send("trip data working...");
   let data = new TripData(req.body);
   console.log("in backend req", req.body);
   let result = await data.save();
@@ -70,14 +146,13 @@ app.get("/view-trip/:id", async (req, res) => {
 });
 
 app.get("/my-trip/:key", async (req, res) => {
-  // res.send("my trip working");
   let result = await TripData.find({ userEmail: req.params.key });
   res.send(result);
 });
-
-// app.listen("https://trip-planner-ai-back-end.vercel.app/"||5000);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
